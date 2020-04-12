7a20c02648e918b215f6bce02390f71404c5c763
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-04-12 16:49:49 -0700

    ok its forever broken

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # reason for this is because migration 0003 seems to be breaking for some reason on the rancher container
-python covidstudents/manage.py migrate
-python covidstudents/manage.py migrate student_stories 0003_auto_20200411_1914 --fake
-python covidstudents/manage.py migrate
+# python covidstudents/manage.py migrate
+# python covidstudents/manage.py migrate student_stories 0003_auto_20200411_1914 --fake
+# python covidstudents/manage.py migrate
 
 exec "$@"
